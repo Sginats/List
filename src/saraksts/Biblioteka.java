87814094316662b6a0173953174302139e7e129e
Sginats <Senvaldsniks2008@gmail.com>
2025-12-10 09:12:40 +0200

    o

--- src/saraksts/Biblioteka.java
+++ src/saraksts/Biblioteka.java
@@ -1,9 +1,68 @@
 package saraksts;
 
-public class Biblioteka {
+import java.util.LinkedList;
+import java.util.regex.Pattern;
 
+import javax.swing.JOptionPane;
+
+public class Biblioteka {
+	static String virknesParbaude(String zinojums, String nokl) {
+		String ievade;
+		do {
+			ievade = JOptionPane.showInputDialog(null,zinojums,nokl);
+		}while(Pattern.matches("^[\\p{L} ]+$", ievade));
+		return ievade;
+	}
+	static int skaitlaParbaude(String zinojums, int tips) {
+		String ievade;
+		int skaitlis;
+		while(true) {
+			ievade = JOptionPane.showInputDialog(null, zinojums, "Skaitļa ievade", JOptionPane.INFORMATION_MESSAGE);
+			if (ievade == null)
+				return -1;
+			try {
+				skaitlis = Integer.parseInt(ievade);
+				if (tips == 1 && skaitlis <= 0) {
+					JOptionPane.showMessageDialog(null, "Lūdzu, ievadi pozitīvu skaitli!", "Kļūda",
+							JOptionPane.WARNING_MESSAGE);
+					continue;
+				}
+				return skaitlis;
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Ievadītā vērtība nav derīgs skaitlis!", "Kļūda",
+						JOptionPane.ERROR_MESSAGE);
+				continue;
+			}
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String nosaukums, autors, izdevnieciba, izvelne;
+		int id = 1, skaits, lppSk, indekss;
+		double cena;
+		LinkedList<Gramata> plaukts = new LinkedList<>();
+		String[] darbibas = { "Pievienot grāmatu", "Noņemt grāmatu", "Apskatīt grāmatu", "Iznomāt grāmatu", "Apturēt"};
+		do {
+			izvelne = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Darbību saraksts", JOptionPane.INFORMATION_MESSAGE, null, darbibas, darbibas[0]);
+			
+			if(izvelne == null);
+				izvelne = "Apturēt";
+            switch(izvelne) {
+            case "Pievienot grāmatu":
+            	nosaukums =
+            	virknesParbaude("Ievadi grāmatas nosaukumu:", "Zaļā pasaka");
+            	if(nosaukums == null)
+            		break;
+                autors =
+                virknesParbaude("Ievadi grāmatas autoru:", "Andrējs Upīts");
+                if(autors == null)
+                	break;
+                izdevnieciba =
+                		virknesParbaude("Ievadi grāmatas izdevniecību:", "Zvaigzne ABC");
+                if(izdevnieciba == null)
+                	break;
+                break;
+            }
+		}while(!izvelne.equals("Apturēt"));
 
 	}
 
--- src/saraksts/Gramata.java
+++ src/saraksts/Gramata.java
@@ -1,5 +1,42 @@
 package saraksts;
 
-public class Gramata {
+import java.time.LocalDate;
 
+import javax.swing.JOptionPane;
+
+public class Gramata {
+private int id, skaits, lppSk;
+private String nosaukums, autors, izdevnieciba;
+private LocalDate panemts, atgriezts;
+private double cena;
+
+public Gramata(int id, String nosaukums, String autors, String izdevnieciba, int lppSk, double cena, int skaits) {
+    this.id = id;
+    this.skaits = skaits;
+    this.lppSk = lppSk;
+    this.nosaukums = nosaukums;
+    this.autors = autors;
+    this.izdevnieciba = izdevnieciba;
+    this.cena = cena;
+  }
+
+public String getNosaukums() {
+    return nosaukums;
+}
+public void info() {
+	JOptionPane.showMessageDialog(null, "ID: " + id + "\nNosaukums: " + nosaukums + "\nAutors: " + autors +
+            "\nIzdevniecība: " + izdevnieciba + "\nLappušu skaits: " + lppSk + "\nCena: " + cena + " EUR");
+}
+public void panemtGramatu() {
+	if (skaits > 0) {
+		skaits--;
+		panemts = LocalDate.now();
+		atgriezts = panemts.plusMonths(1);
+		JOptionPane.showMessageDialog(null,
+				"Grāmata '" + nosaukums + "' ir veiksmīgi paņemta!\nAtgriešanas datums: " + atgriezts);
+	} else {
+		JOptionPane.showMessageDialog(null, "Diemžēl grāmata '" + nosaukums + "' nav pieejama pašlaik.", "Paziņojums",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+}
 }
